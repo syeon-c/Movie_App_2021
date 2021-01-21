b115e09b4f216af30eec3ea74a0066ca1bb4bb2f
최서연 <gkdltjdus@gmail.com>
2021-01-21 19:29:07 +0900

    #2.0 JSX & PROPS Completed!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { waitForDomChange } from "@testing-library/react"
+import React from "react"
+
+function Food({ name, picture }) {
+  return <div>
+    <h2>I like { name }</h2>
+    <img src = { picture }></img> 
+  </div>
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image: 
+      "https://www.maangchi.com/wp-content/uploads/2014/06/whole-cabbage-kimchi.jpg"
+  },
+  {
+    id: 2,
+    name: "Malatang",
+    image: 
+      "https://3.bp.blogspot.com/-WUyJMm6cKUY/XPLxAmFAVpI/AAAAAAAA5G4/CgNVF0tTdQ85HYMX7oPnc_igTz-FcIhMgCK4BGAYYCw/s1600/image1-781362.jpeg"
+  },
+  {
+    id: 3,
+    name: "Cake",
+    image: 
+      "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2F1542062283%2Fchocolate-and-cream-layer-cake-1812-cover.jpg%3Fitok%3DR_xDiShk"
+  }
+];
+
+function renderFood(dish) {
+  console.log(dish)
+  return <Food key={ dish.id} name={ dish.name } picture={ dish.image } />
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(renderFood)}
     </div>
   );
 }
