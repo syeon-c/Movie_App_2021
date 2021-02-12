ebaff73df017cf55b230b94d21bff6e012422743
최서연 <gkdltjdus@gmail.com>
2021-02-12 16:44:07 +0900

    #4.1 Rendering the Movies Completed!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import { waitForDomChange } from "@testing-library/react"
 import React from "react"
-import PropTypes from "prop-types"
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
     state = {
       isLoading: true,
       movies: []
     };
+
     getMovies = async () => {
-      const movie  = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json")
-    }
+      const {
+        data: {
+          data: { movies }  
+        }
+      } = await axios.get(
+        "https://yts-proxy.nomadcoders1.now.sh/list_movies.json"
+      );
+      this.setState({ movies, isLoading: false });
+    };
+
     componentDidMount() {
       this.getMovies();
     }
 
     render() {
-      const { isLoading } = this.state;
-      return <div> {isLoading ? "Loading...." : "We are ready"} </div>;
+      const { isLoading, movies } = this.state;
+      return (
+        <div>
+          {isLoading
+            ? "Loading..."
+            : movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+        </div>
+      );
     }
   }
 
